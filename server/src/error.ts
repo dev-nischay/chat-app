8ee7bbbb977a35b3c11a3d27abe61671a315ea69
dev-nischay <nischay.mehn@gmail.com>
2025-12-19 23:56:57 +0530

    added error middle ware

---
+++ server/src/error.ts
@@ -0,0 +1,21 @@
+import type { ErrorRequestHandler } from "express";
+import { AppError } from "./utils.js";
+
+export const error: ErrorRequestHandler = (err: unknown, req, res, next) => {
+  if (err instanceof AppError) {
+    const statusCode = err.statusCode || 500;
+
+    return res.status(statusCode).json({
+      status: false,
+      error: err.message || "Something went wrong",
+    });
+  }
+
+  if (err instanceof Error) {
+    console.log(`Unexpected Error : ${err.message} `);
+    return res.status(500).json({
+      status: false,
+      error: "Internal Server Error",
+    });
+  }
+};
