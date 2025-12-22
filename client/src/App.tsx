2d058c0c7764cfd53b60424d0fa8efe50af0edfd
dev-nischay <nischay.mehn@gmail.com>
2025-12-22 14:21:01 +0530

    updating frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./layout/MainLayout";
 import Dashboard from "./pages/Dashboard";
 import ChatPage from "./pages/Chat";
