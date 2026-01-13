988cbf19dd14b6c0353d3dfa9205f13007654948
dev-nischay <nischay.mehn@gmail.com>
2026-01-13 17:59:58 +0530

    fixed scroll bug

--- client/src/pages/Chat.tsx
+++ client/src/pages/Chat.tsx
@@ -39,10 +39,7 @@ export const ChatPage = () => {
           console.log(response);
 
           if (data.type === "response") {
-            setChats((prev) => [
-              ...prev,
-              { sender: "server", message: data.payload.message },
-            ]);
+            setChats((prev) => [...prev, { sender: "server", message: data.payload.message }]);
           }
           if (data.type === "error") {
             throw new Error(data.payload.message);
@@ -90,34 +87,26 @@ export const ChatPage = () => {
           {/* Logo */}
           <div className="flex items-center gap-2">
             <MessageCircle color="white" size={20} />
-            <div className="text-white text-xl font-medium ">
-              Real Time Chat
-            </div>
+            <div className="text-white text-xl font-medium ">Real Time Chat</div>
           </div>
 
-          <div className="text-white/60  font-thin  text-[13px]">
-            temporary room that expires after both users exit
-          </div>
+          <div className="text-white/60  font-thin  text-[13px]">temporary room that expires after both users exit</div>
 
           {/* Room details */}
           <div className="bg-white/10 rounded-md p-2 flex justify-between items-center my-5 border-white/10 border px-2">
-            <div className="text-white/60 text-[12px] font-thin">
-              Room Code :
-            </div>
+            <div className="text-white/60 text-[12px] font-thin">Room Code :</div>
             <div className="text-white/60 text-[12px]">Users : 1/2</div>
           </div>
 
           {/* Chat box */}
           {/* enable visible scroll on message */}
-          <div className="border-white/10 overflow-y-scroll  h-[30rem] rounded-xl  border-2 p-3">
+          <div className="border-white/10  h-[30rem] rounded-xl  border-2 p-3">
             <div className="flex flex-col  gap-4">
               {chats.map((e, index) => (
                 <div
                   key={index}
                   className={`min-h-0 p-2 w-fit rounded-md text-sm   ${
-                    e.sender === "server"
-                      ? "bg-white/50 self-start"
-                      : "bg-white self-end"
+                    e.sender === "server" ? "bg-white/50 self-start" : "bg-white self-end"
                   }`}
                 >
                   {e.message}
@@ -134,10 +123,7 @@ export const ChatPage = () => {
               placeholder="Type a message ..."
               ref={inputRef}
             />
-            <button
-              onClick={sendMessage}
-              className="bg-white h-10 w-20 rounded-md text-sm "
-            >
+            <button onClick={sendMessage} className="bg-white h-10 w-20 rounded-md text-sm ">
               Send
             </button>
           </div>
